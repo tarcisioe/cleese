7f00468a46a54dc1bc3f23b5e98bd3ddcc06a958
Tarcisio Eduardo Moreira Crocomo <tarcisio.crocomo@audaces.com.br>
2014-10-15 11:01:38 -0300

    detail on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cleese',
-    packages=find_packages(),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points={
         'console_scripts' : [
